9c99bb9030a16b409c4e4f0572ce64c5a0f79421
Keoni Murray <keonimurray45@gmail.com>
2018-10-28 13:00:55 -0700

    added blueprint for routes

---
+++ app/routes/home.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.get('/', (res, req) => {
+    // This route will handle serving the landing page
+})
+
+router.get('/signup', (req, res) => {
+    /*this route will handle sending user to the page that will ask them what type of user they are */
+})
+
+
+router.get('/login', (req, res) => {
+    //This route will take the user to the login in page
+
+})
