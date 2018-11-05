d6a67529164cf76de84baed889a91a3e24b40ec3
Keoni Murray <keonimurray45@gmail.com>
2018-11-04 17:22:19 -0800

    Donator auth completed

--- app/routes/secure-routes/donators.js
+++ app/routes/secure-routes/donators.js
@@ -40,6 +40,47 @@ router.post('/donatorsignup', (req, res) => {
 
 router.post('/donatorlogin', (req, res) => {
     //this route will handle user login
+    Donators.findOne({
+            email: req.body.email
+        })
+        .exec()
+        //models name is donator not donators
+        .then(donator => {
+
+            bycrypt.compare(req.body.password, donator.password, (err, result) => {
+
+                if (err) {
+                    return res.status(401).json({
+                        failed: "Unauthorized Access"
+                    });
+                }
+                if (result) {
+                    const JWTToken = jwt.sign({
+                            email: donator.email,
+                            _id: donator._id
+                        },
+
+                        'secrect', {
+                            expiresIn: '2h'
+                        });
+                    return res.status(200).json({
+                        sucess: 'user assigned jwt auth',
+                        token: JWTToken
+                    });
+                }
+
+                return res.status(401).json({
+                    failed: 'Unauthorized Access'
+                });
+
+            });
+
+        }).catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
+
 })
 
 router.get('/donatorProfile', (req, res) => {
